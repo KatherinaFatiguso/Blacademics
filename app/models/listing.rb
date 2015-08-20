31b76d5e20b9fefd06b705d00fe24ce8472d9bf1
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2015-08-20 23:09:35 +1000

    fixed the datetimepicker and the start_time and end_time validations.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,28 @@ class Listing < ActiveRecord::Base
 
   has_many :listing_audiences
   has_many :audiences, through: :listing_audiences
+
+  # validate :start_time, presence: true
+  # validate :end_time, presence: true
+  validate :start_time_cannot_be_empty
+  validate :end_time_cannot_be_empty
+  validate :start_time_cannot_be_in_the_past
+  validate :end_time_cannot_be_in_the_past
+
+  def start_time_cannot_be_empty
+    errors.add(:start_time, "Start Time can't be empty") if !start_time.present?
+  end
+
+  def end_time_cannot_be_empty
+    errors.add(:end_time, "End Time can't be empty") if !end_time.present?
+  end
+
+  def start_time_cannot_be_in_the_past
+    errors.add(:start_time, "Start Time can't be in the past") if start_time.present? && start_time < Date.today
+  end
+
+  def end_time_cannot_be_in_the_past
+    errors.add(:end_time, "End Time can't be in the past") if end_time.present? && end_time < Date.today
+  end
+
 end
