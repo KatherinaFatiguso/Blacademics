bc060e48267dc5977fd0136e31a7a7651d0aa6c9
ksilitonga@hotmail.com <ksilitonga@hotmail.com>
2015-09-14 16:21:08 +1000

    update

--- db/migrate/20150812022454_fix_default_value1.rb
+++
@@ -1,9 +0,0 @@
-class FixDefaultValue1 < ActiveRecord::Migration
-  def change
-    change_column :categories, :highschool, :boolean, :default => false
-    change_column :categories, :undergraduate, :boolean, :default => false
-    change_column :categories, :postgraduate, :boolean, :default => false
-    change_column :categories, :employment, :boolean, :default => false
-    change_column :categories, :community, :boolean, :default => false
-  end
-end
